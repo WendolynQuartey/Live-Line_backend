f9cc05e762f91a0fdb221517b4ded30dbcc1dd1f
WendolynQuartey <quarteywendolyn@gmail.com>
2026-03-04 20:04:09 -0500

    initial set up

---
+++ server.js
@@ -0,0 +1,19 @@
+// Imports
+import express from "express";
+import dotenv from "dotenv";
+import corse from "cors";
+
+// Setups
+dotenv.config();
+const app = express();
+
+
+// Middleware
+
+// Routes
+
+
+// Global Error Handling Middleware
+
+
+// Listener
